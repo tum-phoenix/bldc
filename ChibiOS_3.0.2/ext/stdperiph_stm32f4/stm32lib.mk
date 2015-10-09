0475fe77e9631421746eb586bb91b4d147b60fed
Markus Herb <mail@markusherb.de>
2015-10-09 22:51:28 +0200

    Add I2C StdPeriphLib

--- ChibiOS_3.0.2/ext/stdperiph_stm32f4/stm32lib.mk
+++ ChibiOS_3.0.2/ext/stdperiph_stm32f4/stm32lib.mk
@@ -4,6 +4,7 @@ STM32SRC = 	${CHIBIOS}/ext/stdperiph_stm32f4/src/misc.c \
 			${CHIBIOS}/ext/stdperiph_stm32f4/src/stm32f4xx_dma.c \
 			${CHIBIOS}/ext/stdperiph_stm32f4/src/stm32f4xx_exti.c \
 			${CHIBIOS}/ext/stdperiph_stm32f4/src/stm32f4xx_flash.c \
+			${CHIBIOS}/ext/stdperiph_stm32f4/src/stm32f4xx_i2c.c \
 			${CHIBIOS}/ext/stdperiph_stm32f4/src/stm32f4xx_rcc.c \
 			${CHIBIOS}/ext/stdperiph_stm32f4/src/stm32f4xx_syscfg.c \
 			${CHIBIOS}/ext/stdperiph_stm32f4/src/stm32f4xx_tim.c \
