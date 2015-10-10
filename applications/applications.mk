b57a407567247217766c4011577a983d339153cb
Markus Herb <mail@markusherb.de>
2015-10-10 12:15:17 +0200

    Add I2C App sources to makefile

--- applications/applications.mk
+++ applications/applications.mk
@@ -4,6 +4,7 @@ APPSRC =	applications/app.c \
 			applications/app_sten.c \
 			applications/app_gurgalof.c \
 			applications/app_uartcomm.c \
-			applications/app_nunchuk.c
+			applications/app_nunchuk.c \
+			applications/app_i2c.c
 
 APPINC = applications
